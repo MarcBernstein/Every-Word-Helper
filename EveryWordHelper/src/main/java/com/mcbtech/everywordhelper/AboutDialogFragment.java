6e9c3e4a78dee111ab9cba860606426fc4bb6d24
Marc Bernstein <github@marcanderica.org>
2014-08-13 18:49:49 -0700

    Upgrade to latest version of Android Studio

---
+++ EveryWordHelper/src/main/java/com/mcbtech/everywordhelper/AboutDialogFragment.java
@@ -0,0 +1,48 @@
+/* Copyright 2010 Marc Bernstein
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.mcbtech.everywordhelper;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+/**
+ * @author Marc Bernstein (github@marcanderica.org)
+ */
+public class AboutDialogFragment extends SherlockDialogFragment {
+
+	public static final String TAG = AboutDialogFragment.class.getSimpleName();
+
+	public static AboutDialogFragment newInstance() {
+		return new AboutDialogFragment();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.about, container, false);
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		Dialog dialog = super.onCreateDialog(savedInstanceState);
+		dialog.setTitle(R.string.about_title);
+		return dialog;
+	}
+}
